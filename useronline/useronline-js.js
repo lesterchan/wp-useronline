4ccbe044c63d78af6a8835d97ae2cbfab79f8b0b
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-01-10 10:37:12 +0000

    Fixed useronline_ajax_url Bug
    git-svn-id: http://plugins.svn.wordpress.org/wp-useronline/trunk@7280 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- useronline/useronline-js.js
+++ useronline/useronline-js.js
@@ -36,7 +36,7 @@ function useronline_init() {
 
 // Get UserOnline Count
 function get_useronline_count() {
-	useronline_count = new sack(ajax_url);
+	useronline_count = new sack(useronline_ajax_url);
 	useronline_count.setVar("useronline_mode", 'useronline_count');
 	useronline_count.method = 'GET';
 	useronline_count.element = 'useronline-count';
@@ -47,7 +47,7 @@ function get_useronline_count() {
 
 // Get Users Browsing Site
 function get_useronline_browsingsite() {
-	useronline_browsingsite = new sack(ajax_url);
+	useronline_browsingsite = new sack(useronline_ajax_url);
 	useronline_browsingsite.setVar("useronline_mode", 'useronline_browsingsite');
 	useronline_browsingsite.method = 'GET';
 	useronline_browsingsite.element = 'useronline-browsing-site';
@@ -58,7 +58,7 @@ function get_useronline_browsingsite() {
 
 // Get Users Browsing Page
 function get_useronline_browsingpage() {
-	useronline_browsingpage = new sack(ajax_url);
+	useronline_browsingpage = new sack(useronline_ajax_url);
 	useronline_browsingpage.setVar("useronline_mode", 'useronline_browsingpage');
 	useronline_browsingpage.method = 'GET';
 	useronline_browsingpage.element = 'useronline-browsing-page';
