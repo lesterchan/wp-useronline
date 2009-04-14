564818956a0c47c8129e9ed911d2362fc187bc02
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-14 08:50:28 +0000

    Fixed Widget To Work With WP2.8 Bleeding
    git-svn-id: http://plugins.svn.wordpress.org/wp-useronline/trunk@110399 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-useronline.php
+++ wp-useronline.php
@@ -865,6 +865,91 @@ function useronline_page_general_stats($content) {
 	return $content;
 }
 
+### Class: WP-UserOnline Widget
+ class WP_Widget_UserOnline extends WP_Widget {
+	// Constructor
+	function WP_Widget_UserOnline() {
+		$widget_ops = array('description' => __('WP-UserOnline users online statistics', 'wp-useronline'));
+		$this->WP_Widget('useronline', __('UserOnline', 'wp-useronline'), $widget_ops);
+	}
+
+	// Display Widget
+	function widget($args, $instance) {
+		extract($args);
+		$title = attribute_escape($instance['title']);
+		$type = attribute_escape($instance['type']);
+		echo $before_widget.$before_title.$title.$after_title;
+		echo '<ul>'."\n";
+		switch($type) {
+			case 'users_online':
+				get_useronline();
+				break;
+			case 'users_browsing_page':
+				get_users_browsing_page();
+				break;
+			case 'users_browsing_site':
+				get_users_browsing_site();
+				break;
+			case 'users_online_browsing_page':
+				get_useronline();
+				echo '<br />';
+				get_users_browsing_page();
+				break;
+			case 'users_online_browsing_site':
+				get_useronline();
+				echo '<br />';
+				get_users_browsing_site();
+				break;
+		}
+		echo '</ul>'."\n";
+		echo $after_widget;
+	}
+
+	// When Widget Control Form Is Posted
+	function update($new_instance, $old_instance) {
+		if (!isset($new_instance['submit'])) {
+			return false;
+		}
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['type'] = strip_tags($new_instance['type']);
+		return $instance;
+	}
+
+	// DIsplay Widget Control Form
+	function form($instance) {
+		global $wpdb;
+		$instance = wp_parse_args((array) $instance, array('title' => __('UserOnline', 'wp-useronline'), 'type' => 'users_online'));
+		$title = attribute_escape($instance['title']);
+		$type = attribute_escape($instance['type']);
+?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'wp-useronline'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('type'); ?>"><?php _e('Statistics Type:', 'wp-useronline'); ?>
+				<select name="<?php echo $this->get_field_name('type'); ?>" id="<?php echo $this->get_field_id('type'); ?>" class="widefat">
+					<option value="users_online"<?php selected('users_online', $type); ?>><?php _e('Users Online Count', 'wp-useronline'); ?></option>
+					<option value="users_browsing_page"<?php selected('users_browsing_page', $type); ?>><?php _e('Users Browsing Current Page', 'wp-useronline'); ?></option>
+					<option value="users_browsing_site"<?php selected('users_browsing_site', $type); ?>><?php _e('Users Browsing Site', 'wp-useronline'); ?></option>
+					<optgroup>&nbsp;</optgroup>
+					<option value="users_online_browsing_page"<?php selected('users_online_browsing_page', $type); ?>><?php _e('Users Online Count & Users Browsing Current Page', 'wp-useronline'); ?></option>
+					<option value="users_online_browsing_site"<?php selected('users_online_browsing_site', $type); ?>><?php _e('Users Online Count & Users Browsing Site', 'wp-useronline'); ?></option>
+				</select>
+			</label>
+		</p>
+		<input type="hidden" id="<?php echo $this->get_field_id('submit'); ?>" name="<?php echo $this->get_field_name('submit'); ?>" value="1" />
+<?php
+	}
+}
+
+
+### Function: Init WP-UserOnline Widget
+add_action('widgets_init', 'widget_useronline_init');
+function widget_useronline_init() {
+	register_widget('WP_Widget_UserOnline');
+}
+
 
 ### Function: Create UserOnline Table
 add_action('activate_wp-useronline/wp-useronline.php', 'create_useronline_table');
