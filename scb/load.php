9168c08190d8a9177032a746e1a6b6b666559b87
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-09 01:31:07 +0000

    update scbLoad
    git-svn-id: http://plugins.svn.wordpress.org/wp-useronline/trunk@238800 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scb/load.php
+++ scb/load.php
@@ -1,42 +1,44 @@
 <?php
+#debug_print_backtrace();
+
 if ( !class_exists('scbLoad3') ) :
 class scbLoad3 {
 
 	private static $candidates;
 	private static $loaded;
+	private static $initial_load;
 
 	static function init($rev, $file, $classes) {
-		$dir = dirname($file);
+		if ( $path = get_option('scb-framework') && !self::$initial_load ) {
+			if ( $path != __FILE__ )
+				include $path;
 
-		self::$candidates[$rev] = $dir;
-
-		self::load($dir . '/', $classes);
-
-		add_filter('pre_update_option_active_plugins', array(__CLASS__, 'reorder'));
-	}
-
-	static function reorder($active_plugins) {
-		krsort(self::$candidates);
-
-		$dir = dirname(plugin_basename(reset(self::$candidates)));
-
-		$found = false;
-		foreach ( $active_plugins as $i => $plugin ) {
-			$plugin_dir = dirname($plugin);
-
-			if ( $plugin_dir == $dir ) {
-				$found = true;
-				break;
-			}
+			self::$initial_load = true;
 		}
 
-		if ( !$found || 0 == $i )
-			return $active_plugins;
+		self::$candidates[$file] = $rev;
 
-		unset($active_plugins[$i]);
-		array_unshift($active_plugins, $plugin);
+		self::load(dirname($file) . '/', $classes);
 
-		return $active_plugins;
+		add_action('deactivate_plugin', array(__CLASS__, 'deactivate'));
+		add_action('update_option_active_plugins', array(__CLASS__, 'reorder'));
+	}
+
+	static function deactivate($plugin) {
+		$plugin = dirname($plugin);
+
+		if ( '.' == $plugin )
+			return;
+
+		foreach ( self::$candidates as $path => $rev )
+			if ( plugin_basename(dirname(dirname($path))) == $plugin )
+				unset(self::$candidates[$path]);
+	}
+
+	static function reorder() {
+		arsort(self::$candidates);
+
+		update_option('scb-framework', key(self::$candidates));
 	}
 
 	private static function load($path, $classes) {
@@ -54,14 +56,14 @@ class scbLoad3 {
 	}
 
 	static function get_info() {
-		krsort(self::$candidates);
+		arsort(self::$candidates);
 
-		return array(self::$loaded, self::$candidates);
+		return array(get_option('scb-framework'), self::$loaded, self::$candidates);
 	}
 }
 endif;
 
-scbLoad3::init(13, __FILE__, array(
+scbLoad3::init(14, __FILE__, array(
 	'scbUtil', 'scbOptions', 'scbForms', 'scbTable', 'scbDebug',
 	'scbWidget', 'scbAdminPage', 'scbBoxesPage',
 	'scbQuery', 'scbRewrite', 'scbCron',
