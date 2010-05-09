830e15cd620ac57b992ef8669621f6ac6d0c7c60
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-09 10:55:18 +0000

    release version 2.72
    git-svn-id: http://plugins.svn.wordpress.org/wp-useronline/trunk@238869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-useronline.php
+++ wp-useronline.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-UserOnline
 Plugin URI: http://wordpress.org/extend/plugins/wp-useronline/
 Description: Enable you to display how many users are online on your Wordpress site
-Version: 2.71
+Version: 2.72
 Author: Lester 'GaMerZ' Chan & scribu
 
 
