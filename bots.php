0bdc2ace2303763c96c763dec7c9a8a80dc0ae7a
scribu <mail@scribu.net>
2012-11-07 03:23:28 +0200

    move bots to separate file

---
+++ bots.php
@@ -0,0 +1,36 @@
+<?php
+
+function useronline_get_bots() {
+	$bots = array(
+		'Google Bot' => 'google',
+		'MSN' => 'msnbot',
+		'BingBot' => 'bingbot',
+		'Alex' => 'ia_archiver',
+		'Lycos' => 'lycos',
+		'Ask Jeeves' => 'jeeves',
+		'Altavista' => 'scooter',
+		'AllTheWeb' => 'fast-webcrawler',
+		'Inktomi' => 'slurp@inktomi',
+		'Turnitin.com' => 'turnitinbot',
+		'Technorati' => 'technorati',
+		'Yahoo' => 'yahoo',
+		'Findexa' => 'findexa',
+		'NextLinks' => 'findlinks',
+		'Gais' => 'gaisbo',
+		'WiseNut' => 'zyborg',
+		'WhoisSource' => 'surveybot',
+		'Bloglines' => 'bloglines',
+		'BlogSearch' => 'blogsearch',
+		'PubSub' => 'pubsub',
+		'Syndic8' => 'syndic8',
+		'RadioUserland' => 'userland',
+		'Gigabot' => 'gigabot',
+		'Become.com' => 'become.com',
+		'Baidu' => 'baidu',
+		'Yandex' => 'yandex',
+		'Amazon' => 'amazonaws.com'
+	);
+
+	return apply_filters( 'useronline_bots', $bots );
+}
+
--- core.php
+++ core.php
@@ -61,6 +61,8 @@ class UserOnline_Core {
 	}
 
 	function record( $page_url = '', $page_title = '' ) {
+		require_once dirname(__FILE__) . '/bots.php';
+
 		global $wpdb;
 
 		if ( empty( $page_url ) )
@@ -79,37 +81,7 @@ class UserOnline_Core {
 		$current_user = wp_get_current_user();
 
 		// Check For Bot
-		$bots = array(
-			'Google Bot' => 'google',
-			'MSN' => 'msnbot',
-			'BingBot' => 'bingbot',
-			'Alex' => 'ia_archiver',
-			'Lycos' => 'lycos',
-			'Ask Jeeves' => 'jeeves',
-			'Altavista' => 'scooter',
-			'AllTheWeb' => 'fast-webcrawler',
-			'Inktomi' => 'slurp@inktomi',
-			'Turnitin.com' => 'turnitinbot',
-			'Technorati' => 'technorati',
-			'Yahoo' => 'yahoo',
-			'Findexa' => 'findexa',
-			'NextLinks' => 'findlinks',
-			'Gais' => 'gaisbo',
-			'WiseNut' => 'zyborg',
-			'WhoisSource' => 'surveybot',
-			'Bloglines' => 'bloglines',
-			'BlogSearch' => 'blogsearch',
-			'PubSub' => 'pubsub',
-			'Syndic8' => 'syndic8',
-			'RadioUserland' => 'userland',
-			'Gigabot' => 'gigabot',
-			'Become.com' => 'become.com',
-			'Baidu' => 'baidu',
-			'Yandex' => 'yandex',
-			'Amazon' => 'amazonaws.com'
-		);
-
-		$bots = apply_filters( 'useronline_bots', $bots );
+		$bots = useronline_get_bots();
 
 		$bot_found = false;
 		foreach ( $bots as $name => $lookfor ) {
